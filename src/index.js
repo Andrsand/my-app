b9232fb5f8482347200726f8c7ada79223a5ed68
andrsand <angorko@list.ru>
2022-10-04 21:46:47 +0300

    React Exercises
    * Create exercise app
    * Exercises in infinite-scroll

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from "react";
+import { render } from "react-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const style = {
+  //height: 30,
+  width: 300,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8
+};
+
+class App extends React.Component {
+  state = {
+    items: Array.from({ length: 20 }) // поле items с изначальной длинной 20 строк
+  };
+
+  fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 5 })) // добавляет в массив определенное число новых строк
+      });
+    }, 1500);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>demo: react-infinite-scroll-component</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={this.state.items.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.items.map((i, index) => (
+            <div style={style} key={index}>
+              событие - #{index} !!!
+              <details>
+                <summary></summary>
+                <p>Бендер Родригез</p>
+              </details>
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
